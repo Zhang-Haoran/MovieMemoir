6758cbe5d2eb9489a0659190ba7c872e5bf3d28e
Haoran Zhang <hzha278@student.monash.edu>
2020-05-29 17:45:43 +1000

    updated

--- app/src/main/java/com/example/moviememoir/ScreenController/Signin.java
+++ app/src/main/java/com/example/moviememoir/ScreenController/Signin.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,11 @@ import com.example.moviememoir.Model.Usertable;
 import com.example.moviememoir.R;
 import com.example.moviememoir.ServerConnection.Server;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,6 +32,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Signin extends AppCompatActivity {
 
+    private static final String TAG = "Haoran";
     private String usernameValue;
     private String passwordValue;
     TextView signinScreenLabel;
